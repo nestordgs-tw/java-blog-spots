814e3f57df71f14c4a7f108a8f86df345a237cd8
Pablo Castro <pablocastrog@users.noreply.github.com>
2023-02-28 13:41:53 -0300

    fix: ejemplo lectura propertie

--- src/main/java/com/blogsport/blogspot/controllers/ArticleController.java
+++ src/main/java/com/blogsport/blogspot/controllers/ArticleController.java
@@ -3,6 +3,7 @@ package com.blogsport.blogspot.controllers;
 import com.blogsport.blogspot.entity.Article;
 import com.blogsport.blogspot.service.ArticleServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,21 @@ import java.util.List;
 @RequestMapping("/posts")
 public class ArticleController {
 
-
+    private final Environment env;
     private final ArticleServiceImpl articleService;
 
     @Autowired
-    public ArticleController(ArticleServiceImpl postService) {
+    public ArticleController(ArticleServiceImpl postService, Environment env) {
         this.articleService = postService;
+        this.env = env;
     }
 
     @GetMapping("/hello")
     public String sayHello() {
+        // TODO: ejemplo lectura de properties, se debe importar Enviroment y luego leerlo (BORRAR)
+        String path = env.getProperty("test.var");
+        System.out.println(path);
+
         return "Hello Tester";
     }
 
